1dcce1d4f31268a2ff328906890749152a1d0d2a
mahfuzkhan12 <mahfuzalamkhan85@gmail.com>
2023-07-19 22:33:47 +0600

    leetocode problem: 15. 3Sum solved

---
+++ 3Sum/solution.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[]} nums
+ * @return {number[][]}
+ */
+var threeSum = function(nums) {
+    nums.sort((a, b) => a - b);
+    const ans = [];
+    
+    for (let i = 0; i < nums.length - 2; i++) {
+        if (i === 0 || (i > 0 && nums[i] !== nums[i - 1])) {
+            let left = i + 1;
+            let right = nums.length - 1;
+            const target = 0 - nums[i];
+            
+            while (left < right) {
+                if (nums[left] + nums[right] === target) {
+                    ans.push([nums[i], nums[left], nums[right]]);
+                    
+                
+                    while (left < right && nums[left] === nums[left + 1]) left++;
+                    while (left < right && nums[right] === nums[right - 1]) right--;
+                    
+                    left++;
+                    right--;
+                } else if (nums[left] + nums[right] < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+    }
+    
+    return ans;
+};
