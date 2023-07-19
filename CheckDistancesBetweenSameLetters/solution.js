5f07326666ad5541d824d55f7633e98084858554
mahfuzkhan12 <mahfuzalamkhan85@gmail.com>
2023-07-19 22:31:49 +0600

    leetcode problem: 2399. Check Distances Between Same Letters solved

---
+++ CheckDistancesBetweenSameLetters/solution.js
@@ -0,0 +1,50 @@
+/**
+ * @param {string} s
+ * @param {number[]} distance
+ * @return {boolean}
+ */
+var checkDistances = function(s, distance) {
+    
+    let set = new Set()
+    let obj = {
+        a: 0,
+        b: 1,
+        c: 2,
+        d: 3,
+        e: 4,
+        f: 5,
+        g: 6,
+        h: 7,
+        i: 8,
+        j: 9,
+        k: 10,
+        l: 11,
+        m: 12, 
+        n: 13,
+        o: 14,
+        p: 15,
+        q: 16,
+        r: 17,
+        s: 18,
+        t: 19,
+        u: 20,
+        v: 21,
+        w: 22,
+        x: 23,
+        y: 24,
+        z: 25
+    }
+    
+    for (let i = 0; i < s.length; i++) {
+        
+        if(set.has(s[i])) continue;
+        if(s[i] == s[distance[obj[s[i]]]+1+i]) {
+            set.add(s[i])
+        }else {
+            return false
+        }
+    }
+    
+    return true
+    
+};
