874c4a8deb3c1c3ceabecbc7fb8885766e964790
Mahfuz <72296762+mahfuzkhan12@users.noreply.github.com>
2023-10-30 11:06:32 +0600

    Time: 61 ms (46.49%) | Memory: 44.2 MB (20.05%) - LeetSync

---
+++ 2917-count-pairs-whose-sum-is-less-than-target/count-pairs-whose-sum-is-less-than-target.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number}
+ */
+var countPairs = function(nums, target) {
+    
+    var count = 0
+
+    for(let i = 0; i < nums.length; i++){
+        for(let j = i+1; j < nums.length; j++){
+            if(nums[i] + nums[j] < target){
+                count++
+            }
+        }
+    }
+
+    return count
+
+};
