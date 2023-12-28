ae8d2f9dd0a331a93364943a5632b10a37fbd4b2
Mahfuz <72296762+mahfuzkhan12@users.noreply.github.com>
2023-12-28 15:17:47 +0600

    Time: 126 ms (5.24%) | Memory: 43 MB (33.06%) - LeetSync

---
+++ 1884-minimum-changes-to-make-alternating-binary-string/minimum-changes-to-make-alternating-binary-string.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var minOperations = function(s) {
+    
+    let prev = "0", prev_l = "1"
+
+    let count = s[0] != "0" ? 1 : 0;
+    let count_l = s[0] != "1" ? 1 : 0;
+
+    for(let i = 1; i < s.length; i++){
+        if(prev == s[i]){
+            count++
+            prev = s[i] == "0" ? "1" : "0"
+        }else {
+            prev = s[i]
+        }
+        
+        if(prev_l == s[i]){
+            count_l++
+            prev_l = s[i] == "0" ? "1" : "0"
+        }else {
+            prev_l = s[i]
+        }
+    }
+
+
+    return count < count_l ? count : count_l
+
+};
