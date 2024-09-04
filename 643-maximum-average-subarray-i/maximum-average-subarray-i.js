ce28ebf5b167290ad9018d2bd7d23e829bc4b3c2
Mahfuz <72296762+mahfuzkhan12@users.noreply.github.com>
2024-09-04 16:00:23 +0600

    Time: 83 ms (51.34%) | Memory: 62.7 MB (45.14%) - LeetSync

--- 643-maximum-average-subarray-i/maximum-average-subarray-i.js
+++ 643-maximum-average-subarray-i/maximum-average-subarray-i.js
@@ -5,21 +5,17 @@
  */
 var findMaxAverage = function(nums, k) {
     
-    var arr = [];
     var sum = 0;
 
     for(let i = 0; i < k; i++){
-        arr.push(nums[i])
         sum += nums[i]
     }
     var res = sum / k;
 
     for(let i = k; i < nums.length; i++){
-        const remove = arr[0];
+        const remove = nums[i - k];
         sum -= remove
         sum += nums[i]
-        arr.shift()
-        arr.push(nums[i])
         const average = sum / k;
         res = average > res ? average : res;
     }
