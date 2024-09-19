b82e2d15cb07ed473702e0ba752fd47fc6d67051
Mahfuz <72296762+mahfuzkhan12@users.noreply.github.com>
2024-09-19 13:20:49 +0600

    Time: 54 ms (37.97%) | Memory: 48.8 MB (17.72%) - LeetSync

---
+++ 357-count-numbers-with-unique-digits/count-numbers-with-unique-digits.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var countNumbersWithUniqueDigits = function(n) {
+
+    if (n == 0) return 1;
+    if (n == 1) return 10;
+
+    let k = 9;
+    
+    for (let i = 0; i < n - 1; i++) {
+        k *= (9 - i);
+    }
+    return k + countNumbersWithUniqueDigits(n - 1);
+
+};
