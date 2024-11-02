c49be521f0242db565eae13c6ec3b3e9322dc9d3
Mahfuz <72296762+mahfuzkhan12@users.noreply.github.com>
2024-11-03 01:35:00 +0600

    Time: 59 ms (64.95%) | Memory: 60 MB (69.49%) - LeetSync

---
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var makeFancyString = function(s) {
+    
+    let ans = s[0]
+
+    let charCount = 1;
+    let prevChar = s[0]
+
+    for(let i = 1; i < s.length; i++){
+        if(s[i] === prevChar){
+            if(charCount < 2){
+                ans += s[i]
+            }
+            charCount++
+        }else {
+            prevChar = s[i]
+            charCount = 1
+            ans += s[i]
+        }
+    }
+
+    return ans
+
+};
