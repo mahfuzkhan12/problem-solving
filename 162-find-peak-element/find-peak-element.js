cef04f6f924293f7a9db0571e3413ad59da85661
Mahfuz <72296762+mahfuzkhan12@users.noreply.github.com>
2024-11-05 19:40:32 +0600

    Time: 0 ms (100.00%) | Memory: 49 MB (58.54%) - LeetSync

---
+++ 162-find-peak-element/find-peak-element.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findPeakElement = function(nums) {
+    
+    for(let i = 0; i < nums.length; i++){
+        if((i === 0 || nums[i - 1] < nums[i]) && (i == nums.length - 1 || nums[i+1] < nums[i])){
+            return i
+        }
+    }
+};
