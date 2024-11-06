2a3fcdb8b49f4adbce8d95a8adc51fce5977f283
Mahfuz <72296762+mahfuzkhan12@users.noreply.github.com>
2024-11-06 17:37:45 +0600

    Time: 12 ms (100.00%) | Memory: 53.1 MB (73.68%) - LeetSync

--- 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.js
+++ 3291-find-if-array-can-be-sorted/find-if-array-can-be-sorted.js
@@ -31,8 +31,7 @@ var canSortArray = function(nums) {
 
 
     let n = nums.length;
-    let k = 0;
-    while (k < n) {
+    for (let k = 0; k < n; k++) {
         for (var i = 1; i < n; i++) {
             if (count[i] === count[i - 1] && nums[i] < nums[i - 1]) {
                 var temp = nums[i];
@@ -43,7 +42,6 @@ var canSortArray = function(nums) {
         if (check(nums)) {
             return true;
         }
-        k++;
     }
     return false;
 
