e5260954162374da8efb51b6cae2938365f7199d
Mahfuz <72296762+mahfuzkhan12@users.noreply.github.com>
2024-11-13 10:48:00 +0600

    Time: 173 ms (18.18%) | Memory: 61.4 MB (78.26%) - LeetSync

---
+++ 2699-count-the-number-of-fair-pairs/count-the-number-of-fair-pairs.js
@@ -0,0 +1,37 @@
+/**
+ * @param {number[]} nums
+ * @param {number} lower
+ * @param {number} upper
+ * @return {number}
+ */
+var countFairPairs = function(nums, lower, upper) {
+    nums.sort((a, b) => a - b);
+    let ans = 0;
+  
+    for (let i = 0; i < nums.length - 1; i++) {
+        const low = lowerBound(nums, lower - nums[i], i + 1);
+        const up = upperBound(nums, upper - nums[i], i + 1);
+        ans += up - low;
+    }
+    return ans;
+};
+
+function lowerBound(nums, target, start) {
+    let end = nums.length;
+    while (start < end) {
+        let mid = Math.floor((start + end) / 2);
+        if (nums[mid] >= target) end = mid;
+        else start = mid + 1;
+    }
+    return start;
+}
+
+function upperBound(nums, target, start) {
+    let end = nums.length;
+    while (start < end) {
+        let mid = Math.floor((start + end) / 2);
+        if (nums[mid] > target) end = mid;
+        else start = mid + 1;
+    }
+    return start;
+}
